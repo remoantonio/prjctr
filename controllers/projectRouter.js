b4ec5f4dc2a501cb0d007160dda4a509c494c69f
Remo Gonzalez <remoantonio@gmail.com>
2020-07-09 15:54:36 -0400

    Tasks working on projects, added date field and days until using momentJS

--- controllers/projectRouter.js
+++ controllers/projectRouter.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const moment = require('moment')
 
 ////////////////////////////////////////////////////////////////
 // Models
@@ -13,7 +14,14 @@ const Project = require('../models/projectModel')
 
 // index
 router.get('/', (req, res) => {
-    res.send('Here is the index')
+    Project.find({}, (err, projects) => {
+        if (err) {console.log(err)}
+        // console.log(projects)
+        res.render('../views/project/index.ejs', {
+            listProjects: projects,
+            tabTitle: 'Project List'
+        })
+    })
 })
 
 // create
@@ -23,8 +31,9 @@ router.post('/', (req, res) => {
         if (err){console.log(err)}
         console.log(project)
     })
-    res.redirect('/')
+    res.redirect('/prjctr/project')
 })
+
 // new project
 router.get('/new', (req, res) => {
     res.render('../views/project/newProject.ejs', {
@@ -32,4 +41,29 @@ router.get('/new', (req, res) => {
         tabTitle: 'Create New Project'
     })
 })
+
+// new task
+router.post('/task/:id', (req, res) => {
+    console.log(req.body)
+    console.log(req.params.id)
+    Project.findByIdAndUpdate(req.params.id, {$push : {tasks: req.body}}, {new : true}, (err, project) => {
+        if (err) {console.log(err)}
+        console.log(project)
+        res.redirect('/prjctr/project/' + project.id)
+    })
+})
+
+// show
+router.get('/:id', (req, res) => {
+    Project.findById(req.params.id, (err, project) => {
+        if (err) {console.log(err)}
+        console.log(project)
+        res.render('../views/project/show.ejs', {
+            tabTitle: project.projectName,
+            currentProject: project,
+            moment : moment
+        })
+    })
+})
+
 module.exports = router
--- models/projectModel.js
+++ models/projectModel.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const projectSchema = new mongoose.Schema({
-    name: {type: String, required: true},
+    projectName: {type: String, required: true},
     description: String,
     dueDate: Date,
-    tasks: [{type: String, complete: {type:Boolean, default: false}}],
-    complete: {type: Boolean, default: false}
+    tasks: [{task: String, complete: {type:Boolean, default: false}}],
+    complete: {type: Boolean, default: false},
 })
 
 const Project = mongoose.model('Project', projectSchema)
