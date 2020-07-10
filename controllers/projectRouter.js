39450cd69fbceabd4b875f2963758cae30306adc
Remo Gonzalez <remoantonio@gmail.com>
2020-07-10 12:39:09 -0400

    task edit successful

--- controllers/projectRouter.js
+++ controllers/projectRouter.js
@@ -24,7 +24,7 @@ router.get('/', (req, res) => {
     })
 })
 
-// delete
+// delete project
 router.delete('/:id', (req, res) => {
     Project.findByIdAndDelete(req.params.id, (err, projects) => {
         if (err) {console.log(err)}
@@ -33,7 +33,7 @@ router.delete('/:id', (req, res) => {
 })
 
 
-// create
+// create project
 router.post('/', (req, res) => {
     console.log(req.body)
     Project.create(req.body, (err, project) => {
@@ -65,12 +65,12 @@ router.post('/task/:id', (req, res) => {
 
 // delete task
 router.delete('/task/:id/:task', (req, res) => {
-    // console.log(req.body.taskID)
+    console.log(req.params)
     Project.findById(req.params.id, (err, project) => {
         if (err) {console.log(err)}
         let holder =[]
         for (let i = 0; i < project.tasks.length; i++) {
-            if (project.tasks[i].id != req.body.taskID) {
+            if (project.tasks[i].id != req.params.task) {
                 holder.push(project.tasks[i])
             }
         }
@@ -82,7 +82,8 @@ router.delete('/task/:id/:task', (req, res) => {
         })
     })
 })
-// update
+
+// update project
 router.put('/:id', (req, res) => {
     console.log('update route')
     Project.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, project) => {
@@ -91,7 +92,7 @@ router.put('/:id', (req, res) => {
     })
 })
 
-// edit
+// edit project
 router.get('/:id/edit', (req, res) => {
     Project.findById(req.params.id, (err, project) => {
         res.render('../views/project/newProject.ejs', {
@@ -104,6 +105,54 @@ router.get('/:id/edit', (req, res) => {
     })
 })
 
+// update task
+router.put('/task/:id/:task', (req, res) => {
+    // console.log(req.body)
+    // console.log(req.params)
+    Project.findById(req.params.id, (err, project) => {
+        if (err) {console.log(err)}
+        let holder =[]
+        // console.log("project testing",project)
+        for (let i = 0; i < project.tasks.length; i++) {
+            // console.log(i,'projecttasks',project.tasks[i].id.toString())
+            // console.log('reqparamstask',req.params.task.toString())
+            // let x = `'` + req.params.task.replace(/ /g, '') + `'`
+            // let y = `'` + project.tasks[i].id + `'`
+            // console.log(x)
+            // console.log(y)
+            // console.log(x==y)
+            // console.log(project.tasks[i].id == req.params.task.replace(/ /g, ''))
+            if (project.tasks[i].id == req.params.task.replace(/ /g, '')) {
+                project.tasks[i].task = req.body.task
+                holder.push(project.tasks[i])
+            } else {
+                holder.push(project.tasks[i]);
+            }
+        }
+        // console.log(project.tasks[0].id)
+        console.log("testing",holder)
+        Project.findByIdAndUpdate(req.params.id, {$set: {tasks : holder}}, {new: true}, (err, project) => {
+            if (err) {console.log(err)}
+            res.redirect('/prjctr/project/' + project.id)
+        })
+    })
+})
+
+// edit task
+router.get('/task/:id/:task', (req, res) => {
+    Project.findById(req.params.id, (err, project) => {
+        if (err) {console.log(err)}
+        // console.log(project)
+        res.render('../views/project/show.ejs', {
+            tabTitle: project.projectName,
+            currentProject: project,
+            moment : moment,
+            edit : true,
+            currentTask : req.params.task
+        })
+    })
+})
+
 // show
 router.get('/:id', (req, res) => {
     Project.findById(req.params.id, (err, project) => {
@@ -112,7 +161,8 @@ router.get('/:id', (req, res) => {
         res.render('../views/project/show.ejs', {
             tabTitle: project.projectName,
             currentProject: project,
-            moment : moment
+            moment : moment,
+            edit : false
         })
     })
 })
