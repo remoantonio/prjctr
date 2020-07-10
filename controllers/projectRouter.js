152828223a825a52dc011bc34b0ee58553239256
Remo Gonzalez <remoantonio@gmail.com>
2020-07-10 00:26:31 -0400

    edit and delete for projects funtional. working on task delete

--- controllers/projectRouter.js
+++ controllers/projectRouter.js
@@ -18,11 +18,20 @@ router.get('/', (req, res) => {
         if (err) {console.log(err)}
         // console.log(projects)
         res.render('../views/project/index.ejs', {
-            listProjects: projects,
+            currentProject: projects,
             tabTitle: 'Project List'
         })
     })
 })
+
+// delete
+router.delete('/:id', (req, res) => {
+    Project.findByIdAndDelete(req.params.id, (err, projects) => {
+        if (err) {console.log(err)}
+        res.redirect('/prjctr/project')
+    })
+})
+
 
 // create
 router.post('/', (req, res) => {
@@ -38,7 +47,8 @@ router.post('/', (req, res) => {
 router.get('/new', (req, res) => {
     res.render('../views/project/newProject.ejs', {
         edit: false,
-        tabTitle: 'Create New Project'
+        tabTitle: 'Create New Project',
+        actionForm: '/prjctr/project/',
     })
 })
 
@@ -53,11 +63,32 @@ router.post('/task/:id', (req, res) => {
     })
 })
 
+// update
+router.put('/:id', (req, res) => {
+    Project.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, project) => {
+        if (err) {console.log(err)}
+        res.redirect('/prjctr/project/' + project.id)
+    })
+})
+
+// edit
+router.get('/:id/edit', (req, res) => {
+    Project.findById(req.params.id, (err, project) => {
+        res.render('../views/project/newProject.ejs', {
+            tabTitle : 'Edit Project',
+            currentProject : project,
+            edit : true,
+            moment : moment,
+            actionForm: '/prjctr/project/' + project.id + '?_method=Put',
+        })
+    })
+})
+
 // show
 router.get('/:id', (req, res) => {
     Project.findById(req.params.id, (err, project) => {
         if (err) {console.log(err)}
-        console.log(project)
+        // console.log(project)
         res.render('../views/project/show.ejs', {
             tabTitle: project.projectName,
             currentProject: project,
