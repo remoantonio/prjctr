aa338b7b5b380ccab561f327a39a8dbecb9ea58d
Remo Gonzalez <remoantonio@gmail.com>
2020-07-10 17:28:20 -0400

    user password verify works. need to add to DB and ensure passwor

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -13,9 +13,33 @@ const Project = require('../models/projectModel')
 // Routes
 ////////////////////////////////////////////////////////////////
 
-// index
+// create user
+router.post('/new', (req, res) => {
+    // console.log(req)
+    // console.log(req.body)
+    if (req.body.password == req.body.password2) {
+        req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+        delete req.body.password2
+        console.log(req.body)
+        res.redirect('/')
+    } else {
+        res.redirect('/prjctr/user/new?match=false')
+        // res.render('../views/user/newUser.ejs', {
+        //     tabTitle : 'User Creation',
+        //     match : false
+        // })
+    }
+})
+
+// new user
 router.get('/new', (req, res) => {
-    res.send('working')
+    console.log(false)
+    console.log(req.query.match)
+    console.log(req.query.match == false)
+    res.render('../views/user/newUser.ejs', {
+        tabTitle : 'User Creation',
+        match : req.query.match
+    })
 })
 
 
