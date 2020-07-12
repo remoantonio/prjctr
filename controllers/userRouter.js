586617b05e9d58364a80f478d3c5f0fa0ff3f759
Remo Gonzalez <remoantonio@gmail.com>
2020-07-11 22:22:27 -0400

    completed user creation error handling and assignment of currentUser

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 const express = require('express')
+const sessions= express.Router();
 const router = express.Router()
 
 ////////////////////////////////////////////////////////////////
@@ -17,28 +18,40 @@ const Project = require('../models/projectModel')
 router.post('/new', (req, res) => {
     // console.log(req)
     // console.log(req.body)
-    if (req.body.password == req.body.password2) {
-        req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
-        delete req.body.password2
-        console.log(req.body)
-        res.redirect('/')
-    } else {
-        res.redirect('/prjctr/user/new?match=false')
-        // res.render('../views/user/newUser.ejs', {
-        //     tabTitle : 'User Creation',
-        //     match : false
-        // })
-    }
+    User.findOne({userName: req.body.userName}, (err, user) => {
+        if (err) {console.log(err)}
+        if (user == null) {
+            if (req.body.password == req.body.password2) {
+                req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+                delete req.body.password2
+                // console.log(req.body)
+                User.create(req.body, (err, user) => {
+                    if (err) {
+                        console.log(err)
+                        res.redirect('/prjctr/user/new?problem=Problem with user creation')
+                    } else {
+                        req.session.currentUser = user
+                        console.log("currentUser",req.session.currentUser)
+                        res.redirect('/')    
+                    }
+                })
+            } else {
+                res.redirect('/prjctr/user/new?problem=Passwords do not match')
+            }
+        } else {
+            res.redirect('/prjctr/user/new?problem=User Name is not available')
+        }
+    })
 })
 
 // new user
 router.get('/new', (req, res) => {
-    console.log(false)
-    console.log(req.query.match)
-    console.log(req.query.match == false)
+    // console.log(false)
+    // console.log(req.query.match)
+    // console.log(req.query.match == false)
     res.render('../views/user/newUser.ejs', {
         tabTitle : 'User Creation',
-        match : req.query.match
+        problem : req.query.problem
     })
 })
 
